b56975861a8b7a99d02836ceba5b634da6004c4e
Linus-Jonsson <70511818+Linus-Jonsson@users.noreply.github.com>
2021-08-26 15:08:14 +0200

    Update

---
+++ UnitySammanslaget/Assets/DebugTesting.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugTesting : MonoBehaviour
+{
+    public void DebugNote()
+    {
+        Debug.Log("Button clicked");
+    }
+}
